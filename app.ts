158a8ff4293680a8bf5abb667da154d93bfc4e56
Elin Boström <ebostrom@live.se>
2021-05-05 21:49:08 +0200

    Core Types: Array

--- app.ts
+++ app.ts
@@ -5,7 +5,15 @@
 
 const person = {
   name: 'Elin',
-  age: 27
+  age: 27,
+  hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports']
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
