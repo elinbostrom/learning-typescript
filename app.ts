9857e2956d7a0c382e321237368f46368587b124
Elin Boström <ebostrom@live.se>
2021-05-06 09:59:29 +0200

    Functions Type

--- app.ts
+++ app.ts
@@ -14,8 +14,29 @@ function add(n1: number, n2: number) {
 function printResult(num: number){
   console.log('Result: ' + num);
 }
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2; 
+  cb(result)
+}
+
 // ? A function is not allowed to return the type "undefined"
 // * Void is the standard that you should use. 
 // ! Undefined is not preferable but in some cases you want to return "undefined"
 
 printResult(add(5,12))
+
+// ? Whatever we store in 'combineValues' should be a function
+// let combineValues: Function;
+
+// * To customize the type so you get the right amount of arguments etc. we use Function Types
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8,8));
+
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+})
